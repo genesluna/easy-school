4d3ed7b421cb1dbab7423d0b51b7367c3ff74fd1
genesluna <genesluna@gmail.com>
2024-11-16 11:46:06 -0300

    feat: add product model

--- src/product/models.py
+++ src/product/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    expiration_date = models.DateField()
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        db_table = "product"
+
+    def __str__(self):
+        return f"{self.name}"
