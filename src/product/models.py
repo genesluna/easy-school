fef3a30684b26412314eb3b90683a74bcea29886
genesluna <genesluna@gmail.com>
2024-11-16 15:06:33 -0300

    refactor: add verbose names

--- src/product/models.py
+++ src/product/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    expiration_date = models.DateField()
+    name = models.CharField(max_length=255, verbose_name="Nome")
+    description = models.TextField(verbose_name="Descrição")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Preço")
+    expiration_date = models.DateField(verbose_name="Data de Validade")
 
     class Meta:
-        verbose_name = "Product"
-        verbose_name_plural = "Products"
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
         db_table = "product"
 
     def __str__(self):
