cab034c330f06c71e0f379a1e292c3a88ef2de26
genesluna <genesluna@gmail.com>
2024-11-16 15:44:58 -0300

    refactor: add verbose names

--- src/course/models.py
+++ src/course/models.py
@@ -5,13 +5,13 @@ from src.student.models import Student
 
 class Course(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    students = models.ManyToManyField(Student, related_name="courses")
+    title = models.CharField(max_length=255, verbose_name="Título")
+    description = models.TextField(verbose_name="Descrição")
+    students = models.ManyToManyField(Student, related_name="courses", verbose_name="Alunos")
 
     class Meta:
-        verbose_name = "Course"
-        verbose_name_plural = "Courses"
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
         db_table = "course"
 
     def __str__(self):
--- src/student/models.py
+++ src/student/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class Student(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100, verbose_name="Nome")
+    lastname = models.CharField(max_length=100, verbose_name="Sobrenome")
+    email = models.EmailField(unique=True, verbose_name="Email")
 
     class Meta:
-        verbose_name = "Student"
-        verbose_name_plural = "Students"
+        verbose_name = "Aluno"
+        verbose_name_plural = "Alunos"
         db_table = "student"
 
     def __str__(self):
