5b9aa46f1e70ed7eae77221e8df03c11b841c570
genesluna <genesluna@gmail.com>
2024-11-16 11:48:46 -0300

    feat: add products to admin console

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from src.product.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Product)
