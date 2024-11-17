3636e8f897d610fc5a1a5eb408700c557db2ac84
genesluna <genesluna@gmail.com>
2024-11-17 20:40:18 -0300

    fix: concatened installed apps with third partty ones

--- src/settings.py
+++ src/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 
 THIRD_PARTY_APPS = ["rest_framework"]
 
+INSTALLED_APPS += THIRD_PARTY_APPS
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
